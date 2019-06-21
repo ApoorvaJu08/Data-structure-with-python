9f7bd600039268a19a03a9936b996f5b474a0f56
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-21 07:38:02 +0530

    list details program

---
+++ Infytq/list_details.py
@@ -0,0 +1,19 @@
+import sys
+# This function provides the capacity, size and space left in the list. It can be invoked to get details about the list
+def list_details(lst):
+    #Number of elements that can be stored in the list
+    print("Capacity: ", (sys.getsizeof(lst) - 64)//8)
+    #Number of elements in the list
+    print("Size: ", len(lst))
+    #Number of elements that can be stored in space left
+    print("Space left: ", ((sys.getsizeof(lst) - 64) - len(lst*8))//8)
+
+    #(size - 36)/4 for 32 bit machines and
+    #(size - 64)/8 for 64 bit machines
+    #36, 64 - size of an empty list based on machine
+    #4, 8 - size of a single element in the list based on machine
+
+marias_list = []
+print("Empty list created:")
+print("List details: ")
+list_details(marias_list)
