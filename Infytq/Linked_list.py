87e98d3779fabf715d1150969930c8a5928bdab2
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-25 08:16:49 +0530

    untracked

---
+++ Infytq/Linked_list.py
@@ -0,0 +1,8 @@
+class LinkedList:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+    def get_head(self):
+        return self.__head
+    def get_tail(self):
+        return self.__tail
